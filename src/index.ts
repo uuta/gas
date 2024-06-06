da6134e77eea819dfd35b947669b1ed719b3b3ba
uuta <nightrage.metal66@gmail.com>
2024-06-06 17:45:34 +0900

    Add initial project files and configurations

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+function main() {
+  scoring();
+}
+
+function sheet(name) {
+  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  return spreadsheet.getSheetByName(name);
+}
+
+function scoring() {
+  const logsSheet = sheet("logs");
+  const scoresSheet = sheet("scores");
+  const categoryRelationsSheet = sheet("category_relations");
+  const categoriesSheet = sheet("categories");
+
+  const logs = logsSheet.getDataRange().getValues();
+  const categoryRelations = categoryRelationsSheet.getDataRange().getValues();
+  const categories = categoriesSheet.getDataRange().getValues();
+  const categoryRelationsHeader = categoryRelations.shift();
+  const scoreHeader = scoresSheet.getDataRange().getValues().shift();
+
+  categoryRelations.forEach(function (categoryRelation, i) {
+    categories.forEach(function (category) {
+      if (categoryRelation[2] === category[0]) {
+        categoryRelations[i] = Array.from(
+          new Set([...categoryRelation, ...category]),
+        );
+      }
+    });
+  });
+
+  // logs to objects
+  const logsHeaders = logs.shift();
+  const objLogs = [];
+  for (var i = 1; i < logs.length; i++) {
+    const row = logs[i];
+    const obj = {};
+    for (var j = 0; j < logsHeaders.length; j++) {
+      obj[logsHeaders[j]] = row[j];
+    }
+    objLogs.push(obj);
+  }
+
+  // categoryRelations to objects
+  const categoryRelationsHeaders = logs.shift();
+  const objCategoryRelations = {};
+  for (var i = 1; i < categoryRelations.length; i++) {
+    const row = categoryRelations[i];
+    const obj = {
+      countName: row[3],
+      categoryName: row[2],
+    };
+    objCategoryRelations[row[1]] = obj;
+  }
+
+  const score = {};
+  scoreHeader.forEach((row) => (score[row] = 0));
+  Logger.log(score);
+
+  objLogs.forEach((l) => {
+    Object.entries(l).forEach(([key, value]) => {
+      if (objCategoryRelations[key] === undefined) {
+        return;
+      }
+    });
+  });
+}
